ac6331fee0b83ffa67595b7224e22a8830a3fd04
77chigata <Ochigata316@gmail.com>
2025-07-04 15:56:29 +0000

    ajour

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: false,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  user: any;
+  async ngOnInit() {
+    await this.getUserInfo();
+  }
+constructor(private authService: AuthService, private router: Router) {}
+  async getUserInfo(): Promise<void> {
+    this.authService.getUserUtilisateur().subscribe((data) => {
+      this.user = data;
+      localStorage.setItem('user', JSON.stringify(this.user));
+      console.log(this.user);
+    });
+  }
+  hasRole(id: number): boolean {
+    return this.user?.roles?.some((role: any) => role.idRole === id);
+  }
 }
